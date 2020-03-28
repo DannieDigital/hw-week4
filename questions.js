0ae5297458c4202031743377beca4e060c524764
Danielle Beaty <daniellerbeaty@gmail.com>
2020-03-28 14:05:36 -0400

    Homework (v3.0)

--- questions.js
+++
@@ -1,7 +0,0 @@
-var questions = [
-    {
-      title: "HTML stand for:",
-      choices: ["How To Meet Ladies", "Holy Turkey Monkey Language", "Hyper Text Markup Language", "Hyper Touch Markup Language"],
-      answer: "Hyper Text Markup Language"
-    },  
-  ]
--- script.js
+++ script.js
@@ -30,4 +30,10 @@ function setTime() {
   }
   
 
-
+  var questions = [
+    {
+      title: "HTML stand for:",
+      choices: ["How To Meet Ladies", "Holy Turkey Monkey Language", "Hyper Text Markup Language", "Hyper Touch Markup Language"],
+      answer: "Hyper Text Markup Language"
+    },  
+  ]

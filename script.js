9497c1a260ddf1d6d9b779f177007ee827d8fcb5
Danielle Beaty <daniellerbeaty@gmail.com>
2020-04-22 01:02:37 -0400

    Homework (v4.0)

--- script.js
+++ script.js
@@ -1,39 +1,95 @@
-var i = 0;
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $(this).hide();
+    $(".rules").hide();
+
+});
+});
+
+
+//create a variable that hold the questions section from the html
+var quizQuestions = document.querySelector("#quiz")
+
+var quiz = [
+  {
+    q: "HTML stand for:",
+    choices: ["How To Meet Ladies", "Holy Turkey Monkey Language", "Hyper Text Markup Language", "Hyper Touch Markup Language"],
+    a: "Hyper Text Markup Language", },
+
+  {  q: "CSS means:",
+    choices: ["Cute Sandy Storm", "Cascading Style Sheet", "Candy Sugar & Spice", "Country Style Steak"],
+    a: "Cascading Style Sheet"
+  },  
+
+  {  q: "This symbol => is a ",
+    choices: ["Javascript Function", "Equal to but greater than", "It is a symbol", "XML symbol"],
+    a: "Javascript Function"
+  },  
+  
+]
+
+
+// We start the game with a score of 0.
 var score = 0;
-var secondsLeft = 60;
-var timeEl = document.querySelector("#time")
-var messageEl = document.querySelector("#message")
-var storedScores;
-var scoreList = [];
-var answerOne = document.getElementById("");
-var answerTwo = document.getElementById("");
-var answerThree = document.getElementById("");
-var answerFour = document.getElementById("");
+function startQuiz(quiz) {
+  for (var i = 0; i < quiz.length; i++) {
 
-// Set timer for quiz and alert user when time has ran out. Used example from 08-Ins_Timers-Intervals
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = "Timer " + secondsLeft;
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        alert("Sorry, you are out of time")
-        questionEnder();
-      }
+    // Each time we print the value inside the array.
+   var answer = console.log(quiz[i].q);
 
-      else if (i === questions.length) {
-        clearInterval(timerInterval);
-      }
-    }, 1000);
-    return (score)
+    if ((quiz[0].a === "Hyper Text Markup Language" )) {
+      score++;
+      // will change to document.write
+        console.log("Correct!");
+
   }
-  
 
-  var questions = [
-    {
-      title: "HTML stand for:",
-      choices: ["How To Meet Ladies", "Holy Turkey Monkey Language", "Hyper Text Markup Language", "Hyper Touch Markup Language"],
-      answer: "Hyper Text Markup Language"
-    },  
-  ]
+  else {
+    score--;
+    console.log("Wrong!");
+  }
+
+  if ((quiz[1].a === "Cascading Style Sheet" )) {
+      score++;
+        console.log("Correct!");
+
+  }
+
+  else {
+    score--;
+    console.log("Wrong!");
+  }
+
+  if ((quiz[2].a === "Javascript Function" )) {
+    score++;
+      console.log("Correct!");
+
+}
+
+else {
+  score--;
+  console.log("Wrong!");
+}
+
+
+}
+}
+
+startQuiz(quiz)
+
+var timerEl = document.querySelector("#time")
+timerSeconds = setInterval(clockTick, 5000);
+
+ 
+timerEl.textContent = timerSeconds;
+
+function clockTick() {
+   // update time
+   time--;
+   timerEl.textContent = time;
+   // check if user ran out of time
+   if (time <= 0) {
+     quizEnd();
+   }
+ }
